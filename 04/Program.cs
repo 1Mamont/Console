426b5540a9e03a180d612d039d9065dd64e05ac3
1Mamont <n.lesenok@gmail.com>
2023-07-09 19:06:21 +0300

    if else -tolower

---
+++ 04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Люблю тебя :) ");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
